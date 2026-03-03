a9c25e1c45a782a33e57faf5319cdfcb7ee6e2db
vishal kumar <vishal.kumar.0000666@gmail.com>
2026-03-03 18:35:47 -0500

    Install dependencies tensor flow   tensor flow facemesh mode and react webcam and also imported them

--- facemesh/src/App.js
+++ facemesh/src/App.js
@@ -1,5 +1,14 @@
+//install dependencies. done 
+//import dependencies. done
+
 import logo from './logo.svg';
 import './App.css';
+import React,{useRef} from 'react';
+// this will allow us to use the webcam and ref to access the webcam stream
+import * as tf from '@tensorflow/tfjs';
+import * as facemesh from '@tensorflow-models/facemesh';
+import webcam from 'react-webcam';
+import { useRef } from 'react';
 
 function App() {
   return (
