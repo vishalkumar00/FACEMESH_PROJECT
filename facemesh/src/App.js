1e2a0770abb5e5ffbdfb7f835395e8c6c066483b
vishal kumar <vishal.kumar.0000666@gmail.com>
2026-03-03 19:13:20 -0500

    load the facemesh model

--- facemesh/src/App.js
+++ facemesh/src/App.js
@@ -1,6 +1,7 @@
 //install dependencies. done 
 //import dependencies. done
 // set up webcam and canvas. done
+//load facemesh model. done
 
 import logo from './logo.svg';
 import './App.css';
@@ -15,6 +16,58 @@ function App() {
   // set up webcam and canvas refs
   const webcamRef =useRef(null);
   const canvasRef = useRef(null);
+
+  // load facemesh model
+
+  //Why async? Because loading a neural network takes time. It downloads model weights (basically learned numbers) from the internet.
+  const runFacemesh = async ()=>{
+    // facemesh.load()
+// This loads the pretrained FaceMesh neural network into memory.
+
+// await means:
+//“Pause this function until the model finishes loading.”
+
+//Without await, JavaScript would move on immediately before the model is ready.
+    const net = await facemesh.load({
+
+      // inputResolution
+
+// This tells the model what size image it should process.
+
+// Why does that matter?
+
+// Neural networks don’t see pixels like humans. They see matrices.
+// Bigger image → more pixels → more math → slower.
+
+// So:
+
+// 900x600 → detailed but heavier computation
+
+// Smaller resolution → faster but less precise
+
+// You’re basically deciding:
+// “Do I want sharper detection or better performance?”
+//       inputResolution:{width:900,height:600},
+      scale:0.8
+
+// scale: 0.8
+
+// This rescales the input before processing.
+
+// If scale = 1
+// → uses full resolution
+
+// If scale = 0.5
+// → processes half-size image (faster, less accurate)
+
+// Scale 0.8 means:
+// “Shrink the image slightly to speed things up while keeping decent accuracy.”
+
+// It’s a performance dial.
+    })
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
