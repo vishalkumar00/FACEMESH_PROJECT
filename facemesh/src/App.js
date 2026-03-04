79a16c807890c7c01699df28e7e948a6d9534a92
vishal kumar <vishal.kumar.0000666@gmail.com>
2026-03-03 19:03:10 -0500

    setup a web cam and canvas for the facemesh model

--- facemesh/src/App.js
+++ facemesh/src/App.js
@@ -1,5 +1,6 @@
 //install dependencies. done 
 //import dependencies. done
+// set up webcam and canvas. done
 
 import logo from './logo.svg';
 import './App.css';
@@ -7,25 +8,45 @@ import React,{useRef} from 'react';
 // this will allow us to use the webcam and ref to access the webcam stream
 import * as tf from '@tensorflow/tfjs';
 import * as facemesh from '@tensorflow-models/facemesh';
-import webcam from 'react-webcam';
-import { useRef } from 'react';
+import Webcam from 'react-webcam';
+
 
 function App() {
+  // set up webcam and canvas refs
+  const webcamRef =useRef(null);
+  const canvasRef = useRef(null);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam
+          ref={webcamRef}
+          style={{
+            width: "900px",
+    height: "600px",
+    borderRadius: "30px",
+    border: "6px solid #ff00cc",
+    backgroundColor: "black",
+    boxShadow: `
+      0 0 20px #ff00cc,
+      0 0 40px #ff00cc,
+      0 0 20px #ffd000,
+      0 0 60px #ffd000
+    `,
+    display: "block",
+    margin: "auto",
+    padding: "6px"
+          }}>
+        </Webcam>
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+   
+    width: "900px",
+    height: "600px",
+    borderRadius: "30px",
+    pointerEvents: "none",}}>
+        </canvas>
       </header>
     </div>
   );
